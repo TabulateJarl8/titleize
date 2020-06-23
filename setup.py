dfc39ef6fb822dd53607f36d98901ba2c4d974b0
TabulateJarl8 <tabulatejarl8@gmail.com>
2020-06-23 14:31:08 -0400

    fixy

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="titleize",
-    version="0.0.2",
+    version="0.0.3",
     author="Tabulate",
     author_email="tabulatejarl8@gmail.com",
     description="Convert Strings to Title Case",
